edce747fccccb14f9505d98fe40e915317700727
rian <rian.koots@stonybrook.edu>
2023-09-07 15:55:38 -0400

    Change name to main

--- pyqcams/util.py
+++ pyqcams/util.py
@@ -125,10 +125,10 @@ def numToV(file):
     return num_x, num_V, num_dV, num_re
 
 if __name__ == '__main__':
-    from pyqcams import pymar
+    from pyqcams import main
     import matplotlib.pyplot as plt
-    calc = pymar.start('example/h2_ca/inputs.json')
-    traj = pymar.QCT(**calc)
+    calc = main.start('example/h2_ca/')
+    traj = main.QCT(**calc)
     bd = bound(traj.v2, 1,traj.mu12, traj.re1)
     print(bd)
     x = np.linspace(1,13,500)
